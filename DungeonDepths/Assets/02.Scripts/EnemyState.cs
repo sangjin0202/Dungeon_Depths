3ca0ae1cf9c63e33c7c7006a76505ff437e3f68f
junorange <37794363+junorange@users.noreply.github.com>
2023-03-20 03:35:03 +0900

    # Enemy _ 적 Base 및 기본 상태 구현 + wayPoint : Sqr 연산자 수정 - Base 기본 변수 및 기능 구현 - State 공통 상태 구현

--- DungeonDepths/Assets/02.Scripts/EnemyState.cs
+++ DungeonDepths/Assets/02.Scripts/EnemyState.cs
@@ -1,67 +1,58 @@
 using UnityEngine;
 
-namespace EmemyState 
+namespace EmemyState
 {
-  class Idle : State<EnemyBase>
-  {
-    public override void Enter(EnemyBase e) 
-    {
-      e.idleTime = Time.time - e.prevTime;
-    }
-    public override void Execute(EnemyBase e) 
-    {
-      e.ExitIdle();
-    }
-    public override void Exit(EnemyBase e) {}
-  }
+	class Idle : State<EnemyBase>
+	{
+		public override void Enter(EnemyBase e)
+		{
+			e.IdleTime = Time.time - e.PrevTime;
+			e.Search();
+		}
+		public override void Execute(EnemyBase e)
+		{
+			e.CheckIdleState();
+		}
+		public override void Exit(EnemyBase e) { }
+	}
 
-  class Patroll : State<EnemyBase> 
-  {
-    public override void Enter(EnemyBase e) 
-    {
-    }
-    public override void Execute(EnemyBase e) 
-    {
-    }
-    public override void Exit(EnemyBase e) 
-    {
-    }
-  }
-
-  class Trace : State<EnemyBase> 
-  {
-    public override void Enter(EnemyBase e) 
-    {
-    }
-    public override void Execute(EnemyBase e) 
-    {
-    }
-    public override void Exit(EnemyBase e) 
-    {
-    }
-  }
-  class Attack : State<EnemyBase> 
-  {
-    public override void Enter(EnemyBase e) 
-    {
-    }
-    public override void Execute(EnemyBase e) 
-    {
-    }
-    public override void Exit(EnemyBase e) 
-    {
-    }
-  }
-  class Die : State<EnemyBase> 
-  {
-    public override void Enter(EnemyBase e) 
-    {
-    }
-    public override void Execute(EnemyBase e) 
-    {
-    }
-    public override void Exit(EnemyBase e) 
-    {
-    }
-  }
+	class Patroll : State<EnemyBase>
+	{
+		public override void Enter(EnemyBase e)
+		{
+			e.wayPoints.MoveNextPoint();
+		}
+		public override void Execute(EnemyBase e)
+		{
+			e.MoveAndRotate(e.wayPoints.destinationPoint);
+			e.CheckPatrolState();
+		}
+		public override void Exit(EnemyBase e) {}
+	}
+	class Trace : State<EnemyBase>
+	{
+		public override void Enter(EnemyBase e){}
+		public override void Execute(EnemyBase e)
+		{
+			e.CheckTraceState();
+		}
+		public override void Exit(EnemyBase e) {}
+	}
+	class Attack : State<EnemyBase>
+	{
+	//Attack범위를 벗어나면 : trace상태로 change
+		public override void Enter(EnemyBase e) {}
+		public override void Execute(EnemyBase e)
+		{
+			e.CheckAttackState();
+			// 공격하는 거
+		}
+		public override void Exit(EnemyBase e) {}
+	}
+	class Die : State<EnemyBase>
+	{
+		public override void Enter(EnemyBase e) {}
+		public override void Execute(EnemyBase e) {}
+		public override void Exit(EnemyBase e) {}
+	}
 }
