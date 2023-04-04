d21d9a18c8b524485d3dd648eb7f0ca38b2b7ef5
junorange <37794363+junorange@users.noreply.github.com>
2023-04-04 21:10:27 +0900

    # 적_ 스크립트 명 변경, 프리팹 수정

--- DungeonDepths/Assets/02.Scripts/Enemy/MeleeAttackMonster.cs
+++ DungeonDepths/Assets/02.Scripts/Enemy/MeleeAttackMonster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class meleeAttackMonster : MonsterBase
+public class MeleeAttackMonster : MonsterBase
 {
     protected override void Awake()
     {
