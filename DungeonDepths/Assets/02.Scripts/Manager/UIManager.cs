b0961d8d577f6819244d05725a644d3d050a58f3
junorange <37794363+junorange@users.noreply.github.com>
2023-04-20 21:35:52 +0900

    # UI_ UIManager 버튼 함수 및 창 프리팹 추가 + Loading 창 추가 + GameOver 프리팹 버튼 추가 + UIManager 버튼 함수들 추가

--- DungeonDepths/Assets/02.Scripts/Manager/UIManager.cs
+++ DungeonDepths/Assets/02.Scripts/Manager/UIManager.cs
@@ -10,12 +10,20 @@ public class UIManager : MonoSingleton<UIManager>
     List<GameObject> windowList = new List<GameObject>();
     GameObject curWindow;
 
-    
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         InitWindowList();
     }
-    void InitWindowList()
+    void InitWindowList()   // 초기화
     {
         var windows = transform.GetChild(0);
         for (int i = 0; i < windows.transform.childCount; i++)
@@ -24,29 +32,58 @@ public class UIManager : MonoSingleton<UIManager>
             windowList[i].SetActive(false);
         }
         curWindow = windowList[0];
+        curWindow.SetActive(true);
     }
-    public void OnWindow(Window _name)
+    public void OnWindow(Window _name) // Full Window 켜기
     {
         windowList[(int)_name].SetActive(true);
         curWindow = windowList[(int)_name];
+        GameManager.Instance.Pause();
     }
-    public void OffWindow(Window _name)
+    public void OffWindow(Window _name) // Full Window 끄기
     {
         windowList[(int)_name].SetActive(false);
         curWindow = null;
+        GameManager.Instance.Resume();
     }
-    public void OnClickOff()
+    public void OnClickCloseBtn()   // UI 종료 버튼
     {
         if (curWindow != null)
             curWindow.SetActive(false);
+        GameManager.Instance.Resume();
     }
-    public void OnClickOptionBtn()
+    public void OnClickOptionBtn()  // 옵션
     {
         OnWindow(Window.OPTION);
     }
+    public void OnClickRestartBtn()   // 게임 오버-> restart 버튼 
+    {
+        OffWindow(Window.GAMEOVER);
+        GameManager.Instance.LoadMenuScene();
+        OnWindow(Window.MAINMENU);
+    }
+    public void OnClickMainMenuPlayBtn() // 메인메뉴에서 플레이 하는 버튼
+    {
+        OffWindow(Window.MAINMENU);
+        StartCoroutine(LoadingWindow());
+        GameManager.Instance.LoadPlayScene();
 
+    }
+
+    IEnumerator LoadingWindow() 
+    {
+        windowList[(int)Window.LOADING].SetActive(true);
+        yield return null;
+        windowList[(int)Window.LOADING].SetActive(false);
+    }
     public void ShowMapInfo()
     {
 
     }
+
+    public void ShowCardInfo(CardData _cardData)
+    {
+
+    }
+    
 }
